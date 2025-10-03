f4d0745842de0ea1bb71709682006f5f0c4763b0
User-PC <2356532@mail.ru>
2025-10-03 15:08:44 +0500

    ДЗ 13 поправлено

--- src/Order.java
+++ src/Order.java
@@ -15,26 +15,26 @@ public class Order {
         return "Покупатель: " + customer + " Корзина: " + Arrays.toString(basket);
     }
 
-    @Override
+   @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Order order = (Order) o;
-        return Objects.equals(customer, order.customer) && arraysEqual(basket, order.basket);
+      if (this == o) return true;
+       if (o == null || getClass() != o.getClass()) return false;
+       Order order = (Order) o;
+       return Objects.equals(customer, order.customer) && arraysEqual(basket, order.basket);
     }
 
-    private boolean arraysEqual(Product[] arr1, Product[] arr2) {
-        if (arr1 == arr2) return true;
-        if (arr1 == null || arr2 == null) return false;
-        if (arr1.length != arr2.length) return false;
+    private boolean arraysEqual(Product[] product1, Product[] product2) {
+        if (product1 == product2) return true;
+        if (product1 == null || product2 == null) return false;
+        if (product1.length != product2.length) return false;
 
-        for (int i = 0; i < arr1.length; i++) {
-            if (arr1[i] == null || arr2[i] == null) {
-                if (arr1[i] != arr2[i]) return false;
-            } else if (!arr1[i].equals(arr2[i])) {
-                return false;
+         for (int i = 0; i < product1.length; i++) {
+            if (product1[i] == null || product2[i] == null) {
+                if (product1[i] != product2[i]) return false;
+            } else if (!product1[i].equals(product2[i])) {
+               return false;
             }
         }
-        return true;
+         return true;
     }
 }
--- src/Product.java
+++ src/Product.java
@@ -21,9 +21,7 @@ public class Product {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
+        if (o == null || getClass() != o.getClass()) return false;
         Product product = (Product) o;
         return id == product.id && Objects.equals(category, product.category);
     }
